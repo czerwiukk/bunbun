53ff65c14be6517ff712ff4611df977b8ad3e1eb
Adrian Czerwiec <adrian.czerwiec@silevis.com>
2023-01-25 14:40:42 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <section class="flex">
+      <h1 class="text-3xl">Bunbun</h1>
+    </section>
+  );
+};
+
+export default App;
